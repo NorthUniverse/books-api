35727c75c1eae39fbdfc5d225e5c021acda50c92
NorthUniverse <dennis.roshan@gmail.com>
2021-10-16 16:11:21 -0400

    prod build

---
+++ dist/books/dto/book.dto.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.QueryParamDto = exports.CreateBookDto = void 0;
+class CreateBookDto {
+}
+exports.CreateBookDto = CreateBookDto;
+class QueryParamDto {
+}
+exports.QueryParamDto = QueryParamDto;
+//# sourceMappingURL=book.dto.js.map
