2b81e00e30d6adbbbe73367fe0dbf030179252e6
NorthUniverse <dennis.roshan@gmail.com>
2021-10-17 13:01:42 -0400

    docker file update

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN npm run build
 FROM node:16.11.1
 WORKDIR /app
 COPY package.json .
-RUN npm install
+RUN npm install --only=production
 COPY --from=build /app/dist ./dist
 CMD npm run start:prod
--- dist/books/book.module.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Book {
-    title: string;
-    authors: string[];
-    description: string;
-    publicationDate: string;
-    infoLink: string;
-    constructor(id: string, title: string, authors: string[], description: string, publicationDate: string, infoLink: string);
-}
